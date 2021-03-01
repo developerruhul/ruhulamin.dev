1fdb2dd0976718218261cc8a3079d909a8c80889
ruhulamindev <ruhulamin.webdev@gmail.com>
2021-03-01 10:22:51 +0600

    config: add a serif font for title to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      taviraj: ['Taviraj', 'serif']
+    }
   },
   variants: {
     extend: {},
