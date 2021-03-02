af418fd4da43c23c63612175b1a09db7f6134d02
ruhulamindev <ruhulamin.webdev@gmail.com>
2021-03-02 16:12:47 +0600

    feat: change content of CTA based on breakpoints

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -28,7 +28,8 @@ const Hero = () => {
         </p>
       </article>
       <Link  to="/contact" className='flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200'>
-        Contact me
+        <span className="lg:hidden">Contact me</span>
+        <span className="hidden lg:inline">Let's work together</span>
       </Link>
     </section>
   );
