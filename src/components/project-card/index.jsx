d81e7df92c6d738006fc3af0e1ce957b7490cc48
ruhulamindev <ruhulamin.webdev@gmail.com>
2021-03-10 10:18:06 +0600

    feat: make project-cards responsive

--- src/components/project-card/index.jsx
+++ src/components/project-card/index.jsx
@@ -5,34 +5,24 @@ import './project-card.css';
 const ProjectCard = ({ className = '', img }) => {
   return (
     <article
-      className={`bg-gray-50 hover:bg-purple-50 border transition-colors duration-200 rounded py-8 px-12 lg:p-12 pb-8 group max-w-sm ${className}`}
+      className={`bg-gray-50 hover:bg-purple-50 rounded group max-w-sm ${className}`}
     >
       <div className='project-img-wrapper rounded-md p-5 relative'>
-        <div className='w-2/4 absolute z-0 -right-12 -top-12'>
+        <div className='w-1/4 absolute z-0 -right-2 sm:-right-6 -top-6'>
           <div className='project-img-bubble rounded-full' />
         </div>
-        <div className='w-1/4 absolute z-0 -left-8 bottom-10'>
+        <div className='w-2/12 absolute z-0 -left-4 bottom-16'>
           <div className='project-img-bubble rounded-full' />
         </div>
-        <div className='w-4 absolute z-0 -left-8 bottom-8'>
+        <div className='w-5 absolute z-0 -left-6 bottom-11'>
           <div className='project-img-bubble rounded-full' />
         </div>
         <img className='w-80 relative z-10 rounded-lg' src={img} alt='' />
       </div>
 
-      <section className='mt-12 flex flex-col space-y-2'>
-        <Link
-          className='text-center bg-gray-50 text-gray-900 bg-opacity-60 border-2 text-lg leading-6 font-semibold py-3 px-6 rounded-xl transition-colors duration-200'
-          to='/work'
-        >
-          Details
-        </Link>
-        <Link
-          className='text-center bg-purple-900 hover:bg-purple-700 text-white text-lg leading-6 font-semibold py-3 px-6 border rounded-xl transition-colors duration-200'
-          to='/work'
-        >
-          Live website
-        </Link>
+      <section className='project-buttons mt-8 flex items-center justify-end space-x-2'>
+        <Link to='/work'>Details</Link>
+        <Link to='/work'>Live website</Link>
       </section>
     </article>
   );
--- src/pages/work/projects.jsx
+++ src/pages/work/projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from '../../components/project-card';
 
 const Projects = () => {
   return (
-    <section className='max-w-6xl mt-20 grid grid-cols-1 justify-items-center gap-y-10 md:gap-x-10 md:grid-cols-2 lg:grid-cols-3'>
+    <section className='mt-20 grid grid-cols-1 justify-items-center lg:justify-items-start gap-y-10 md:gap-x-12 md:grid-cols-2 lg:grid-cols-3'>
       {[
         'https://res.cloudinary.com/deirqouhr/image/upload/v1607262312/chrome_Pv1TUDzLXx_1_pyusuk.png',
         'https://res.cloudinary.com/deirqouhr/image/upload/v1607262312/chrome_Pv1TUDzLXx_1_pyusuk.png',
