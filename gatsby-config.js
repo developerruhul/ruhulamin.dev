0f13a923857b0df584c96dc66ba3b30e38f5236c
ruhulamindev <ruhulamin.webdev@gmail.com>
2021-03-19 07:21:32 +0600

    feat: put in meaningful meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Developer Ruhul | Fullstack web developer`,
     titleTemplate: "%s",
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Ruhul is a fullstack web developer with 2 years of experience. He creates outstanding websites and webapps with beautiful and smooth user interfaces but also he's able to develop the backend and then integrate the two(website and API) to create a full blown web application - usually, he does both at the same time. This allows him to build almost any kind of web app or application very quickly.`,
     twitterUsername: `@developerruhul`,
     image: "https://i.imgur.com/VNlq0Oo.png",
     url: "https://ruhulamin.dev",
@@ -38,7 +38,7 @@ module.exports = {
         background_color: `#7C3AED`,
         theme_color: `#7C3AED`,
         display: `minimal-ui`,
-        icon: "src/images/developer-ruhul-brand.png"
+        icon: "src/images/developer-ruhul.jpg"
       },
     }
   ],
